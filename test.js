b20fe59d09a0be5ade51861696da9cd8aad6978f
FelisbinoJose <246760.jose@unibave.net>
2025-12-28 23:30:20 -0300

    test: adiciona testes da solução

---
+++ test.js
@@ -0,0 +1,24 @@
+const { assert } = require('chai');
+const { narcissistic } = require('./solucao');
+
+describe("Example tests", function () {
+
+    function dotest(input, expected) {
+        const actual = narcissistic(input);
+        assert.strictEqual(
+            actual,
+            expected,
+            `Incorrect answer for value=${input}`
+        );
+    }
+
+    it("Narcissistic numbers", function () {
+        dotest(7, true);
+        dotest(153, true);
+    });
+
+    it("Not narcissistic numbers", function () {
+        dotest(122, false);
+        dotest(487, false);
+    });
+});
