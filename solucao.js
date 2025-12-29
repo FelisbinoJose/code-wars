f82fab78761ec9840b9302bb9c7c6d52c33da620
FelisbinoJose <246760.jose@unibave.net>
2025-12-28 23:26:16 -0300

    docs: adiciona instruções sobre o desafio

---
+++ solucao.js
@@ -0,0 +1,17 @@
+const { Module } = require("@sinclair/typebox");
+
+function narcissistic(value) {
+    const str = String(value);
+    const potencia = BigInt(str.length);
+
+    let soma = 0n;
+
+    for (const digito of str) {
+        const d = BigInt(digito);
+        soma += d ** potencia;
+    }
+
+    return soma === BigInt(value);
+}
+
+module.exports = { narcissistic };
